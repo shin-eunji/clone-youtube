ad8529fd7226d77f7fd801b56c145fc7779e869d
shineunji <dmswlckato@gmail.com>
2020-07-23 14:59:31 +0900

    docs: header.scss

--- static/main.js
+++ static/main.js
@@ -105,7 +105,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/extract-text-webpack-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Undefined variable: \\\"$red\\\".\\n        on line 2 of assets/scss/partials/header.scss\\n        from line 5 of assets/scss/styles.scss\\n>>   background-color: $red;\\n\\n   --------------------^\\n\\n    at /Users/shineunji/Desktop/youtube/node_modules/webpack/lib/NormalModule.js:316:20\\n    at /Users/shineunji/Desktop/youtube/node_modules/loader-runner/lib/LoaderRunner.js:367:11\\n    at /Users/shineunji/Desktop/youtube/node_modules/loader-runner/lib/LoaderRunner.js:233:18\\n    at context.callback (/Users/shineunji/Desktop/youtube/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\\n    at Object.callback (/Users/shineunji/Desktop/youtube/node_modules/sass-loader/dist/index.js:55:7)\\n    at Object.done [as callback] (/Users/shineunji/Desktop/youtube/node_modules/neo-async/async.js:8069:18)\\n    at options.error (/Users/shineunji/Desktop/youtube/node_modules/node-sass/lib/index.js:294:32)\");\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
 
 /***/ }),
 
